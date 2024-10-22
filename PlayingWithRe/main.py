68163f93b76ac26f9837d372b1c51305e866c3db
DanIenasescu <dan.ienasescu@itschool.ro>
2024-10-22 20:27:59 +0300

    Update main.py

--- PlayingWithRe/main.py
+++ PlayingWithRe/main.py
@@ -6,9 +6,37 @@ def print_hi(name):
     input_string = "I love PyCharmPyCharm"
     pattern = r"PyCharm"
     print(re.search(pattern, input_string))
+    input_string = "Str. OnePiece, Nr. 13"
+    pattern = r"Str. (\w+)"
+    match = re.search(pattern, input_string)
+    input_string = match.group()
+    pattern = r"[^Str. ](\w+)"
+    print(re.search(pattern, input_string))
 
+
+def find_all_with_re():
+    pattern = r"Py"
+    input_string = "I love PyPyPyCharmuma muma muma"
+    match = re.findall(pattern, input_string)
+    print(re.findall(pattern, input_string))
+
+def split_using_re():
+    pattern = r"\n Test Procedure"
+    input_string = "Test Precondition: abhvfgliuawervbiluaewr \n Test Procedure: avbiawbv"
+    match = re.split(pattern, input_string)
+    pattern = r"Test Precondition: "
+    input_string = match[0]
+    match = re.split(pattern, input_string)
+    print(match)
+
+def replace_using_re():
+    pattern = r"Test"
+    replace_with = r"sleep"
+    input_string = "Test Precondition: abhvfgliuawervbiluaewr \n Test Procedure: avbiawbv"
+    match  = re.sub(pattern, replace_with, input_string)
+    print(match)
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    replace_using_re()
 
-
+# 00000010000001010000000100000000000001111000000001000000101000000000
